af4a54e43347bf11c3e3f11559aa9571bcc213fa
Ivan Tertychnyy <ivantertychnyi@gmail.com>
2015-02-24 15:30:55 +0300

    dirs change

---
+++ ddm/DDM.py
@@ -0,0 +1,76 @@
+import os
+import time
+import sys
+from ddm.DropboxSEPlugin import DropboxSEPlugin
+from ddm.GridSEPlugin import GridSEPlugin
+
+BIN_HOME='/srv/lsm/rrcki-sendjob'
+LOGFILE='/srv/lsm/log/ddm.log'
+SITE_PREFIX = 'srm://sdrm.t1.grid.kiae.ru:8443/srm/managerv2?SFN='
+SITE_DATA_HOME = '/t1.grid.kiae.ru/data/atlas/atlasscratchdisk/rucio'
+
+
+def log(self, msg):
+        try:
+            f = open(LOGFILE, 'a')
+            f.write("%s %s\n" % (time.ctime(), msg))
+            f.close()
+            os.chmod(LOGFILE, 0666)
+        except:
+            pass
+
+def fail(self, errorcode=200, msg=None):
+    if msg:
+        msg='%s %s'%(errorcode, msg)
+    else:
+        msg=str(errorcode)
+    print msg
+    log(msg)
+    sys.exit(errorcode)
+
+def getSURL(self, scope, lfn):
+        #get full surl
+        # /<site_prefix>/<space_token>/rucio/<scope>/md5(<scope>:<lfn>)[0:2]/md5(<scope:lfn>)[2:4]/<lfn>
+        try:
+            # for python 2.6
+            import hashlib
+            hash = hashlib.md5()
+        except:
+            # for python 2.4
+            import md5
+            hash = md5.new()
+
+        correctedscope = "/".join(scope.split('.'))
+        hash.update('%s:%s' % (scope, lfn))
+        hash_hex = hash.hexdigest()[:6]
+        return '%s%s/%s/%s/%s/%s' % (SITE_PREFIX, SITE_DATA_HOME, correctedscope, hash_hex[0:2], hash_hex[2:4], lfn)
+
+class SEFactory:
+    def __init__(self):
+        self.se = SEPlugin()
+
+    def getSE(self, label, params=None):
+        se = self.se
+        try:
+            if label not in ['dropbox', 'grid']:
+                raise AttributeError("Attribute 'label' error: Not found in list")
+
+            if label == 'dropbox':
+                se = DropboxSEPlugin(params)
+
+            if label == 'grid':
+                se = GridSEPlugin(params)
+        except Exception:
+            log(Exception.message)
+
+        return se
+
+class SEPlugin(object):
+    def __init__(self, params=None):
+        print 'SEPlugin initialization'
+
+    def get(self, src, dest, fsize, fsum):
+        raise NotImplementedError("SEPlugin.get not implemented")
+
+    def put(self, src, dest):
+        raise NotImplementedError("SEPlugin.put not implemented")
---
+++ ddm/GridSEPlugin.py
@@ -0,0 +1,24 @@
+import subprocess
+
+from ddm.DDM import BIN_HOME
+
+class GridSEPlugin():
+    def __init__(self, params=None):
+        print 'GridSEPlugin initialization'
+
+        proc = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        env = proc.communicate(". %s/setup.sh; python -c 'import os; print os.environ'" % BIN_HOME)[0][:-1]
+        env = env.split('\n')[-1]
+        import ast
+        self.myenv = ast.literal_eval(env)
+
+    def get(self, src, dest, fsize, fsum):
+        proc = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, env=self.myenv)
+        out = proc.communicate('python %s/utils/get.py --size %s --checksum %s %s %s' % (BIN_HOME, fsize, fsum, src, dest))
+        #print out
+
+    def put(self, src, dest):
+        #os.system('utils/put.py %s %s' % (src, dest))
+        proc = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, env=self.myenv)
+        out = proc.communicate('python %s/utils/put.py %s %s' % (BIN_HOME, src, dest))
+        #print out
