249c7a6de2b9d7e328fa7fdeea7fd741e11df2e2
Ivan Tertychnyy <ivantertychnyi@gmail.com>
2015-04-13 12:57:03 +0300

    +getJobStatus

--- lib/ui/UserIF.py
+++ lib/ui/UserIF.py
@@ -10,9 +10,22 @@ class UserIF:
 
         mq = MQ(host='localhost', exchange='lsm')
         mq.sendMessage(message, routing_key)
+
+    def getJobStatus(self, ids):
+        import userinterface.Client as Client
+        s,o = Client.getJobStatus(ids)
+        result = {}
+        if s != 0:
+            return result
+        for x in o:
+            result[x.PandaID] = x.jobStatus
+        return result
 # Singleton
 userIF = UserIF()
 del UserIF
 
 def senbJob(req, params):
     userIF.sendjob(params)
+
+def getJobStatus(req, ids):
+    userIF.getJobStatus(ids)
